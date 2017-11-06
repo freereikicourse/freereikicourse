a7f559e451613b9c40134c6b700ae9b650eda98a
Reiki <genericstorage2@gmail.com>
2017-11-06 17:02:52 +0000

    Server files.

---
+++ goodies/PageLayout/css/img/hp/email/img/hp/navig/img/hp/titles/img/navig/js/ml.js
@@ -0,0 +1,7 @@
+function em_click(){
+location.href="mailto:info@freereikicourse.com";
+return true; }
+function ml_write(){
+document.write('info@freereikicourse.com');}
+function ml_hp_write(){
+document.write('<img title="info@freereikicourse.com"');}
